48c2c44748ac6359933e3332dd80fed03b415f51
Beerd <b.d.vandestreek@student.utwente.nl>
2022-01-18 15:20:35 +0100

    Add .gitlab-ci.yml

---
+++ java/pentago_client_server/src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+    @Test
+    public void onePlusOneShouldBeTwo() {
+        int sum = 1 + 1;
+        assertEquals(2, sum);
+    }
+}
