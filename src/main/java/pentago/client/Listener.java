e403e9ba27308c2c97648acdd0aead6f12b1fdcc
Traydr <Traydr@protonmail.com>
2022-01-20 09:55:19 +0100

    Started work on client-side work

--- src/main/java/pentago/client/Listener.java
+++ src/main/java/pentago/client/Listener.java
@@ -1,4 +1,40 @@
 package pentago.client;
 
-public class Listener {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.List;
+
+public class Listener implements Runnable{
+    Socket socket = null;
+    BufferedReader br = null;
+
+    Listener(Socket sock) {
+        this.socket = sock;
+        try {
+            this.br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // TODO : Make this parse all commands, not just output them
+        if (this.socket.isClosed()) {
+            return;
+        }
+        try {
+            String output;
+
+            while ((output = br.readLine()) != null) {
+                String[] outputParsed = output.split("~");
+                System.out.println(output);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/pentago/client/Networking.java
+++ src/main/java/pentago/client/Networking.java
@@ -1,9 +1,58 @@
 package pentago.client;
 
-public class Networking {
-    //@ensures \result == 2;
-    //@pure
-    static int oneplusone() {
-        return 1 + 1;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Networking implements Network{
+    Socket socket = null;
+    BufferedWriter bw;
+    BufferedReader br;
+
+    @Override
+    public boolean connect(InetAddress address, int port) {
+        try {
+            this.socket = new Socket(address, port);
+            this.bw = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+            this.br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (!this.socket.isClosed()) {
+                this.socket.close();
+            }
+            this.bw.close();
+            this.br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean sendMessage(String message) {
+        // TODO : Change so more commands are accepted
+        try {
+            bw.write("SAY~" + message + "\n");
+            bw.flush();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            close();
+            return false;
+        }
+    }
+
+    @Override
+    public void addChatListener() {
+    }
+
+    @Override
+    public void removeChatListener() {
     }
 }
