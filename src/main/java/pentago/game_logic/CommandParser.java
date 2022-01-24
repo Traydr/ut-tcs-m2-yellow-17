3060e51c0c1d4c71786ce89acea93637bbc90114
Beerd <b.d.vandestreek@student.utwente.nl>
2022-01-24 11:41:41 +0100

    Add JavaDoc to CommandParser and Mark

--- src/main/java/pentago/game_logic/CommandParser.java
+++ src/main/java/pentago/game_logic/CommandParser.java
@@ -1,6 +1,15 @@
 package pentago.game_logic;
 
 public class CommandParser {
+
+    /**
+     * This method transforms our own coordinates to the format the protocol requires.
+     *
+     * @param quadrant The quadrant of the coordinate
+     * @param x        The horizontal position of the coordinate
+     * @param y        The vertical position of the coordinate
+     * @return
+     */
     public static int clientToServer(int quadrant, int x, int y) {
         int offset;
 
