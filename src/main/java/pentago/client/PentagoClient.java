80c643fcd375d081f3b6c593c0d78223ee4a491e
Beerd <b.d.vandestreek@student.utwente.nl>
2022-01-28 11:45:49 +0100

    Ask the user to reconnect when the client is not connected anymore

--- src/main/java/pentago/client/PentagoClient.java
+++ src/main/java/pentago/client/PentagoClient.java
@@ -87,7 +87,7 @@ public class PentagoClient {
                 System.out.println("ERR: bad connection");
                 System.exit(1);
             }
-        } catch (UnknownHostException e) {
+        } catch (UnknownHostException exception) {
             System.out.println("ERR: no connection");
             System.exit(2);
         }
