302699664d189036355c089209966de8dba864d1
Beerd <b.d.vandestreek@student.utwente.nl>
2022-01-28 11:10:55 +0100

    Include not supported message in the help menu

--- src/main/java/pentago/client/PentagoClient.java
+++ src/main/java/pentago/client/PentagoClient.java
@@ -228,7 +228,9 @@ public class PentagoClient {
                              "move [A-D][0-8]\n\tPlaces a piece down a piece on the position",
                              "rotate [A-D][L|R]\n\tRotates a quadrant in a specific direction",
                              "ping\n\tPings the server to see if its still alive",
-                             "chat [message]\n\tsends a message to everyone on the server",
+                             "chat [message]" + (this.serverFeatures.contains("CHAT") ? "" :
+                                                 " (Not supported by this server)") + "\n" +
+                             "\tsends" + " a message to " + "everyone on" + " " + "the" + " server",
                              "whisper [user] [message]\n\tsends a message to a specific person",
                              "help\n\tDisplays this help message",
                              "hint\n\tDisplays a possible move",
