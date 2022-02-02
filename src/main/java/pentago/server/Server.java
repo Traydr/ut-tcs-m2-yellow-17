f45cd1252f0d4ea1eddce5780f8d3f78d2cfad9a
Beerd <b.d.vandestreek@student.utwente.nl>
2022-02-02 11:42:28 +0100

    Ask for new port if bind failed

--- src/main/java/pentago/server/Server.java
+++ src/main/java/pentago/server/Server.java
@@ -1,23 +1,31 @@
 package pentago.server;
 
+import java.net.BindException;
 import java.util.Scanner;
 
 public class Server {
     /**
      * Starts a new server object and listens for a port and quit, for when to stop the server.
+     *
      * @param args
      */
     public static void main(String[] args) {
         SimplePentagoServer server = new SimplePentagoServer();
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("What port should the server listen on (0 for random)?");
-            int port = Integer.parseInt(scanner.nextLine());
-            server.start(port);
-
-            String isClosing = "";
-            while (!isClosing.equals("quit")) {
-                isClosing = scanner.nextLine();
+        boolean validPort = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!validPort) {
+            try {
+                System.out.println("What port should the server listen on (0 for random)?");
+                int port = Integer.parseInt(scanner.nextLine());
+                server.start(port);
+                validPort = true;
+            } catch (BindException e) {
+                System.out.println("Couldn't bind to this port, please enter another one");
             }
+        }
+        String isClosing = "";
+        while (!isClosing.equals("quit")) {
+            isClosing = scanner.nextLine();
         }
         server.stop();
     }
